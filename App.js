5e11362fcc4a5b93f1b962c423a9aa3047d60d3d
Jovellmiranda <jovellmiranda14@gmail.com>
2025-08-24 20:15:22 +0800

    feat: add ProfessorDetail

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
+import ProfessorDetail from "./screens/ProfessorDetail";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,11 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfessorDetail"
+          component={ProfessorDetail}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
