c9ba68a3f1c7c366ae845d07e3b8962a63c9bbaf
Jovellmiranda <jovellmiranda14@gmail.com>
2025-08-25 23:16:55 +0800

    feat: register CommentStatus

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProfessorDetailsScreen from "./screens/ProfessorDetailsScreen";
+import CommentStatusScreen from "./screens/CommentStatus";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +25,11 @@ export default function App() {
         <Stack.Screen
           name="ProfessorDetailsScreen"
           component={ProfessorDetailsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CommentStatus"
+          component={CommentStatusScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
